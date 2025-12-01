d04ce2383105d4ce18e18323daadff187e9c5fc7
suraj vishwakarma <dev.surajv@gmail.com>
2025-12-02 00:43:53 +0530

    feat: enhance configuration management by adding env-required tag for server address

--- cmd/student/main.go
+++ cmd/student/main.go
@@ -1,8 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/smartcraze/student-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to student api ")
+	// load mustload for the configuration file
+	cfg := config.MustLoad()
+	// database setup
+
+	//setup the router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student api"))
+	})
+
+	// setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	fmt.Printf("server is started:  %s", cfg.Addr)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start the server")
+		}
+	}()
+	<-done // blocking
+
+	slog.Info("Shutting Down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+
+	if err != nil {
+		slog.Error("failed to Shutdown server", slog.String("error", err.Error()))
+	}
 
 }
